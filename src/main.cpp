f647ce09a2ad02a170d0adbadb2a820e29bea3ff
Sacha <xsacha@gmail.com>
2014-09-16 00:10:59 +1000

    Update to v1.8.3

--- src/main.cpp
+++ src/main.cpp
@@ -50,7 +50,7 @@ Q_DECL_EXPORT int main(int argc, char *argv[])
     app.setOrganizationName("Qtness");
     app.setOrganizationDomain("qtness.com");
     app.setApplicationName("Sachesi");
-    app.setApplicationVersion("1.8.2");
+    app.setApplicationVersion("1.8.3");
     QNetworkProxyFactory::setUseSystemConfiguration(true);
 
     QQmlApplicationEngine engine;
